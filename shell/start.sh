fb65393ea26465d8430aeef8f70c14dedc12c6b1
1425T <1390275281@q.com>
2025-03-31 19:46:50 +0800

    update start.sh and simulation.launch

--- shell/start.sh
+++ shell/start.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+X_POS=${1:-0}
+Y_POS=${2:-0}
+YAW=${3:-1.57079632679}
+
 export GAZEBO_MODEL_PATH="$(HOME)/models"
 #2D: (SICK)lms1xx, (Hokuyo)ust10, (Hokuyo)usm30, (Velodyne)vlp16, (Velodyne)hdl32e
 export JACKAL_LASER=1
@@ -12,6 +18,9 @@ export JACKAL_LASER_3D_TOPIC="velodyne"
 cd $HOME/ME5413_Final_Project
 
 source devel/setup.bash
+
+
+echo "Start simulation, position: spawn_x=$X_POS, spawn_y=$Y_POS, spawn_Y=$YAW"
 
 roslaunch me5413_bringup simulation.launch spawn_x:=$X_POS spawn_y:=$Y_POS spawn_Y:=$YAW
 
